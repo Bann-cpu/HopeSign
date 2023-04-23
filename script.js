0d42bf2d6f9ba4963ca9ecc9976510626f44f763
Hunter_Bann <71389997+Bann-cpu@users.noreply.github.com>
2023-04-24 01:14:50 +0200

    background library

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ let underlineHome = gsap.to(
   },
   ">"
 );
+
+window.addEventListener("DOMContentLoaded", () => {
+  VANTA.NET({
+    el: "#vanta-element",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    points: 15.00
+    //?backgroundAlpha: 0.0,
+  });
+});
