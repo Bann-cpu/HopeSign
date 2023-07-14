b8d6e6de623e9f5c8bfd945d559ed200a01f5b28
Hunter_Bann <71389997+Bann-cpu@users.noreply.github.com>
2023-07-14 13:49:39 +0200

    architectural configuration

--- javascript-pages/home-page.js
+++ javascript-pages/home-page.js
@@ -1,52 +1,18 @@
-//?Animation de la nav bar
 let navElements = gsap.from(".button", {
-  y: "random(-200, 200)",
-  stagger: 0.5,
-  opacity: 0,
-  duration: 1,
-});
-let homeElements = gsap.from("#home_intro p , #home_intro svg", {
-  y: -100,
-  duration: 0.5,
-  opacity: 0,
-  stagger: 0.5,
-});
-
-//?Animation de l'underline
-let underlineHome = gsap.to(
-  "#underlinehome",
-  {
-    width: 645,
-    ease: "power1.out",
-    duration: 0.5,
-  },
-  ">"
-);
-
-//?ANIMATION DU DONUTS BACKGROUND
-window.addEventListener("DOMContentLoaded", () => {
-  VANTA.TRUNK({
-    el: "#vanta-element",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: true,
-    minHeight: 200.0,
-    minWidth: 200.0,
-    scale: 1.0,
-    scaleMobile: 1.0,
-    backgroundColor: "#23153c",
-    spacing: 1.5,
-    chaos: 4.0,
+    y: "random(-200, 200)",
+    stagger: 0.5,
+    opacity: 0,
+    duration: 1,
   });
-});
+  let homeElements = gsap.from("#home_intro p , #home_intro svg", {
+    y: -100,
+    duration: 0.5,
+    opacity: 0,
+    stagger: 0.5,
+  });
+  
 
-let fiEnv = gsap.to(".icon_envelopper", {
-  x: 165,
-  ease: "steps(12)",
-  duration: 3,
-});
 
-//?ANIMATION DU CURSEUR
 const cursorStyle = document.querySelector(".inner-cursor");
 const cursorSndStyle = document.querySelector(".outer-cursor");
 console.log(cursorSndStyle);
@@ -88,4 +54,21 @@ aArray +
     element.addEventListener("mouseleave", () => {
       cursorStyle.classList.remove("grow");
     });
+  });
+
+
+  let underlineHome = gsap.to(
+    "#underlinehome",
+    {
+      width: 645,
+      ease: "power1.out",
+      duration: 0.5,
+    },
+    ">"
+  );
+  
+  let fiEnv = gsap.to(".icon_envelopper", {
+    x: 165,
+    ease: "steps(12)",
+    duration: 3,
   });
