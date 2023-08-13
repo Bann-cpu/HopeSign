4674f1e67cb3fe5b5a84a46d36590a64666a7456
Hunter_Bann <71389997+Bann-cpu@users.noreply.github.com>
2023-08-13 14:23:42 +0200

    finalisation of carousel

--- javascript-pages/home-page.js
+++ javascript-pages/home-page.js
@@ -80,4 +80,12 @@ var flickityCarousel = new Flickity(carousselElement, {
   autoPlay: true,
   pauseAutoPlayOnHover: true,
   pageDots: true, 
+  percentPosition: true,
+
+  arrowShape: { 
+    x0: 10,
+    x1: 60, y1: 50,
+    x2: 70, y2: 40,
+    x3: 30
+  }
 });
